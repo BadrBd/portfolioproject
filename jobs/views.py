fcea556c5bb8c8f8d14783ba2d2736abc75ea37f
Bidouli <bidoulibadreddine@gmail.com>
2020-12-04 18:35:14 +0100

    added bootstrap

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Job
 
 # Create your views here.
+def Home(request):
+	jobs = Job.objects.all()
+	context = {
+	'object_list':jobs
+	}
+
+	return render(request,'jobs/home.html',context)
